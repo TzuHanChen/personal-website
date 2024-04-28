b3b34ff05933cd63072e9045a729e9b5e3d3c233
TzuHan Chen <45888769+TzuHanChen@users.noreply.github.com>
2024-04-28 22:32:24 +0800

    安裝各項套件

--- components/fonts.ts
+++ components/fonts.ts
@@ -1,5 +1,4 @@
-import { Noto_Sans_TC } from 'next/font/google';
-import { Noto_Serif_TC } from 'next/font/google';
+import { Noto_Sans_TC, Noto_Serif_TC, Noto_Color_Emoji, Fira_Code } from 'next/font/google';
  
 export const NotoSansTC = Noto_Sans_TC({
 	subsets: ['latin'], 
@@ -8,6 +7,17 @@ export const NotoSansTC = Noto_Sans_TC({
 });
 
 export const NotoSerifTC = Noto_Serif_TC({
+	subsets: ['latin'], 
+	weight: ['400', '700'],
+	display: 'swap',
+});
+
+export const NotoColorEmoji = Noto_Color_Emoji({
+	weight: ['400'],
+	display: 'swap',
+});
+
+export const FiraCode = Fira_Code({
 	subsets: ['latin'], 
 	weight: ['400', '700'],
 	display: 'swap',
