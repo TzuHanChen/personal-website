420164af7c039f0c6875ff50b06e61a32bbc597d
hahachentzuhan <hahachentzuhan@gmail.com>
2024-07-11 06:34:51 +0000

    更換圖示

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
         <h1 className="text-9xl">文字測試</h1>
-        <span className="material-symbols-outlined">home</span>
-        <span className="material-symbols-outlined fill">home</span>
+        <p className="text-5xl">文字測試</p>
       </div>
     </main>
   );
--- ui/fonts.ts
+++ ui/fonts.ts
@@ -1,24 +1,25 @@
 import { Noto_Sans_TC, Noto_Serif_TC, Noto_Color_Emoji, Fira_Code } from 'next/font/google';
- 
+
 export const NotoSansTC = Noto_Sans_TC({
-	subsets: ['latin'], 
+	subsets: ['latin'],
 	weight: ['400', '700'],
 	display: 'swap',
 });
 
 export const NotoSerifTC = Noto_Serif_TC({
-	subsets: ['latin'], 
+	subsets: ['latin'],
 	weight: ['400', '700'],
 	display: 'swap',
 });
 
 export const NotoColorEmoji = Noto_Color_Emoji({
+	subsets: ['emoji'],
 	weight: ['400'],
 	display: 'swap',
 });
 
 export const FiraCode = Fira_Code({
-	subsets: ['latin'], 
+	subsets: ['latin'],
 	weight: ['400', '700'],
 	display: 'swap',
 });
