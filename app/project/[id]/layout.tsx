eb40792fe6f938a767bbf8ad61a20c2b5a3939b0
hahachentzuhan <hahachentzuhan@gmail.com>
2024-08-12 11:18:59 +0000

    調整專案版面

--- app/project/[id]/layout.tsx
+++ app/project/[id]/layout.tsx
@@ -1,6 +1,6 @@
 function ProjectSideBar({ projectId }: { projectId: string }) {
   return (
-    <div className="py-24 px-6 flex flex-col gap-6 md:px-24 lg:sticky lg:w-96 lg:py-36 lg:px-12">
+    <div className="py-24 px-6 flex flex-col gap-6 md:px-24 lg:sticky lg:top-0 lg:w-96 lg:h-screen lg:py-36 lg:px-12">
       <h1 className="text-3xl text-gray-900 md:text-4xl lg:text-5xl">專案資訊 {projectId}</h1>
       <p>專案描述專案描述專案描述專案描述專案描述專案描述專案描述</p>
       <button className="w-max self-end">DEMO</button>
@@ -11,7 +11,7 @@ function ProjectSideBar({ projectId }: { projectId: string }) {
 export default function ProjectLayout({ children, params }:
   { children: React.ReactNode, params: { id: string } }) {
   return (
-    <main className="bg-gray-50 md:ml-20 selection:bg-teal-600 selection:text-gray-50">
+    <main className="min-h-screen bg-gray-50 md:ml-20 selection:bg-teal-600 selection:text-gray-50 lg:flex">
       <ProjectSideBar projectId={params.id} />
       {children}
     </main>
