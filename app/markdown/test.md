72c1426e15645bb8b337599db8dec1e23bb5e413
tzuhan_chen <tzuhan@doubleservice.com>
2024-08-16 17:17:34 +0800

    新增 markdown 內容區塊

---
+++ app/markdown/test.md
@@ -0,0 +1,13 @@
+## h2
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at facilis ab voluptatum dolor repudiandae itaque? Autem, ipsam beatae incidunt aliquid mollitia sed laudantium illo quod dignissimos quas reprehenderit reiciendis!
+
+### h3
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at facilis ab voluptatum dolor repudiandae itaque? Autem, ipsam beatae incidunt aliquid mollitia sed laudantium illo quod dignissimos quas reprehenderit reiciendis!
+
+## h2
+
+### h3
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at facilis ab voluptatum dolor repudiandae itaque? Autem, ipsam beatae incidunt aliquid mollitia sed laudantium illo quod dignissimos quas reprehenderit reiciendis!
--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,9 +1,10 @@
+import Content from "@/app/ui/content";
 import Members from "@/app/ui/member";
 import Timeline from "@/app/ui/timeline";
 import Output from "@/app/ui/output";
 import PageButton from "@/app/ui/page-button";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function ProjectPage({ params }: { params: { id: string } }) {
   const memberData = [
     { name: "我", role: "網站後端", skill: "XAMPP, PHP, MySQL" },
     { name: "同學", role: "網站前端", skill: "Vue.js" },
@@ -13,22 +14,16 @@ export default function Page({ params }: { params: { id: string } }) {
 
   return (
     <section className="py-24 px-6 flex flex-col gap-12 md:px-24 md:gap-16 lg:flex-1 lg:py-36 lg:px-12 lg:gap-24 xl:px-24">
-      <p>page {params.id}</p>
-
       <img src="" alt="專案主視覺" title="專案主視覺"
         className="mx-auto w-full max-w-192 aspect-video rounded-3xl bg-gray-300" />
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at facilis ab voluptatum dolor repudiandae itaque? Autem, ipsam beatae incidunt aliquid mollitia sed laudantium illo quod dignissimos quas reprehenderit reiciendis!</p>
-
       <Members data={memberData} />
-
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at facilis ab voluptatum dolor repudiandae itaque? Autem, ipsam beatae incidunt aliquid mollitia sed laudantium illo quod dignissimos quas reprehenderit reiciendis!</p>
 
       <Timeline start="2020/07" period="一個月" end="2020/08" />
 
       <Output items={["uxr", "flow", "ui", "pro", "page", "site"]} />
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at facilis ab voluptatum dolor repudiandae itaque? Autem, ipsam beatae incidunt aliquid mollitia sed laudantium illo quod dignissimos quas reprehenderit reiciendis!</p>
+      <Content filename={params.id} />
 
       <div className="w-full flex flex-col gap-6 md:flex-row *:flex-1">
         <PageButton title="專案名稱" subtitle="上一個專案" align="left" href="" />
