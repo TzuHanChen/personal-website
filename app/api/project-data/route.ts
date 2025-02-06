c1be75dc451b4f65d50aea5f6ef5329ecf590473
TzuHan Chen <hahachentzuhan@gmail.com>
2025-02-07 00:18:10 +0800

    新增 dashboard 專案資料

--- app/api/project-data/route.ts
+++ app/api/project-data/route.ts
@@ -48,6 +48,28 @@ const data = [
       "end": "進行中"
     },
     "outputs": ["flow", "ui", "pro", "site"]
+  },
+  {
+    "slug": "dashboard",
+    "name": "儀表板",
+    "description": "依照 Next.js 官方教學課程實作的網站",
+    "skill": "Next.js, Tailwind CSS, clsx, TypeScript, Zod, Auth.js",
+    "link": {
+      "title": "GitHub repo",
+      "subtitle": "原始碼",
+      "href": "https://github.com/tzuhanchen/nextjs-dashboard",
+      "outside": true
+    },
+    "keyVisual": "dashboard.png",
+    "members": [
+      { name: "我", role: "網站前端" },
+    ],
+    "timeline": {
+      "start": "2023/12",
+      "period": "兩個月",
+      "end": "2024/02"
+    },
+    "outputs": ["site"]
   }
 ];
 
