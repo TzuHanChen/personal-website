18cb357244d258b8086b4479ef41d351357bfb80
TzuHan Chen <hahachentzuhan@gmail.com>
2025-03-09 00:00:11 +0800

    更新首頁主視覺、關於我主視覺、技能描述

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
+import { FolderIcon, ArrowDownTrayIcon } from '@heroicons/react/24/outline';
 import PageButton from "@/app/ui/page-button"
 import Card from "@/app/ui/card";
 import { Project } from "@/app/type";
@@ -8,6 +10,11 @@ export const metadata: Metadata = {
   title: "陳子涵",
   description: "陳子涵的個人網站",
   openGraph: {
+    type: "website",
+    url: "https://personal-website-tzuhanchen.vercel.app",
+    title: "陳子涵",
+    description: "陳子涵的個人網站",
+    siteName: "陳子涵",
     images: process.env.NEXT_PUBLIC_FRONTEND_URL + '/image/personal-website.png'
   }
 };
@@ -16,20 +23,30 @@ export const dynamic = 'force-dynamic';
 
 function Hero() {
   return (
-    <section className="rounded-4xl bg-white py-12 px-6 flex flex-col gap-6">
-      <div className="flex flex-col justify-center items-center gap-6 md:flex-row">
+    <section className="rounded-4xl bg-white py-12 px-6 flex flex-col gap-9">
+      <div className="mx-auto w-full max-w-144 flex flex-col justify-center items-center gap-6 lg:justify-start lg:items-start">
         <Image src="/image/icon.svg" alt="陳子涵的圖示" title="陳子涵的圖示"
           width={96} height={96} className="size-24 rounded-full" />
         <div>
-          <h1 className="mb-1 text-2xl text-center text-gray-900 md:text-3xl">
+          <h1 className="mb-3 text-3xl font-bold text-center text-gray-900 md:text-4xl lg:text-left">
             你好，我是陳子涵</h1>
-          <p className="text-2xl text-center text-gray-900 md:text-3xl">
+          <p className="text-xl text-center text-gray-900 md:text-2xl lg:text-left">
             現在是前端工程師</p>
         </div>
       </div>
-      <p className="mx-auto w-full max-w-144 text-gray-600">
-        {"　　"}我目前使用 Next.js, React, Tailwind CSS, React Hook Form 等工具，完成公司任務與業餘專案的各項畫面與功能。之前有接觸過介面設計、使用者體驗、後端開發，現在仍有持續利用過往經歷，與負責這些職位的夥伴討論、溝通與合作。如果你想找我聊聊新的合作機會，請聯繫我！
+      <p className="mx-auto w-full max-w-144 text-gray-600 leading-relaxed">
+        我目前使用 Next.js, React, Tailwind CSS, TypeScript 等工具，完成公司任務與業餘專案的各項畫面與功能。之前有接觸過介面設計、使用者體驗、後端開發，現在仍有持續利用過往經歷，與負責這些職位的夥伴討論、溝通與合作。如果你想找我聊聊新的合作機會，請聯繫我！
       </p>
+      <div className="mx-auto w-full max-w-144 flex justify-center gap-6 flex-wrap lg:justify-start">
+        <Link href="/project" className="rounded-full bg-gray-800 py-3 px-6 flex gap-1.5 text-white active:bg-teal-700 transition-colors duration-300">
+          <FolderIcon className="size-6" />
+          <span>瀏覽我的專案</span>
+        </Link>
+        <Link href="/TzuHanChen_Resume_20250308.pdf" target="_blank" className="rounded-full border border-gray-700 py-3 px-6 flex gap-1.5 active:bg-gray-100 transition-colors duration-300">
+          <ArrowDownTrayIcon className="size-6" />
+          <span>下載我的履歷</span>
+        </Link>
+      </div>
     </section>
   )
 }
