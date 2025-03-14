83ad9a9e47297b000807ba967b1b1dbf10f01a3c
hahachentzuhan <hahachentzuhan@gmail.com>
2025-03-14 10:58:43 +0000

    更新關於我頁面的自我介紹

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ function Hero() {
         </div>
       </div>
       <p className="mx-auto w-full max-w-144 text-gray-600 leading-relaxed">
-        我目前使用 Next.js, React, Tailwind CSS, TypeScript 等工具，完成公司任務與業餘專案的各項畫面與功能。之前有接觸過介面設計、使用者體驗、後端開發，現在仍有持續利用過往經歷，與負責這些職位的夥伴討論、溝通與合作。如果你想找我聊聊新的合作機會，請聯繫我！
+        我目前使用 Next.js, React, Tailwind CSS, TypeScript 等工具，完成公司任務與業餘專案的網頁切板與功能實作。在學期間有接觸過介面設計、使用者體驗、後端開發，現在仍有持續利用過往經歷，與負責這些職位的夥伴討論、溝通與合作。如果你想找我聊聊新的合作機會，請聯繫我！
       </p>
       <div className="mx-auto w-full max-w-144 flex justify-center gap-6 flex-wrap lg:justify-start">
         <Link href="/project" className="rounded-full bg-gray-800 py-3 px-6 flex items-center gap-1.5 hover:bg-teal-700 active:bg-teal-700 transition-colors duration-300 group">
@@ -82,7 +82,7 @@ function About() {
     <section className="mx-auto max-w-144 flex flex-col gap-6 lg:gap-9">
       <h2 className="text-4xl text-gray-900 md:text-5xl">關於我</h2>
       <p className="text-gray-700">
-        參與過產品開發流程的各項工作後，我選擇以前端工程作為本人職業。<br />
+        參與過產品開發流程的各項任務後，我選擇以前端工程作為本人職業。<br />
         期許自己能夠利用過往經驗，與來自各領域的同事順利合作。</p>
       <PageButton title="自我介紹" href="/about" />
     </section>
@@ -91,7 +91,7 @@ function About() {
 
 export default function Home() {
   return (
-    <main className="bg-gray-50 py-24 px-6 flex flex-col gap-16 md:ml-20 md:px-24 lg:py-36 lg:gap-24">
+    <main className="bg-gray-50 py-24 px-6 flex flex-col gap-16 md:ml-20 md:px-24 lg:gap-24">
       <Hero />
       <Projects />
       <About />
