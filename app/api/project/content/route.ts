1c26115c9337af724377f5807ca72389b4eece37
TzuHan_Chen <hahachentzuhan@gmail.com>
2025-08-05 08:53:57 +0000

    refactor: 刪除不需要的程式碼

--- app/api/project/content/route.ts
+++ app/api/project/content/route.ts
@@ -1,7 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { sql } from '@/lib/db-init';
-import { getBaseUrl } from "@/lib/url";
-import { Project } from "@/lib/types";
 
 export async function GET(request: NextRequest) {
   const req = request.nextUrl.searchParams;
@@ -10,18 +8,7 @@ export async function GET(request: NextRequest) {
   if (!slug) {
     return NextResponse.json({ message: "slug not provided" }, { status: 404 });
   } else {
-    const result = await sql`
-      SELECT * FROM projects WHERE slug = ${slug}
-    `;
-
-    // const baseUrl = getBaseUrl();
-    // const res = await fetch(`${baseUrl}/api/project-data`,
-    //   { headers: { 'Content-Type': 'application/json' } }
-    // ).then(res => res.json());
-
-    // const project = res.find((project: Project) => {
-    //   return project.slug === slug;
-    // });
+    const result = await sql`SELECT * FROM projects WHERE slug = ${slug}`;
 
     if (result.length > 0) {
       const project = result[0], projectId = Number.parseInt(result[0].id);
@@ -45,9 +32,9 @@ export async function GET(request: NextRequest) {
       const next = await sql`
         SELECT slug, name FROM projects WHERE id = ${nextId}
       `;
-      return Response.json({ ...project, skills, members, next: next[0], prev: prev[0] }, { status: 200 });
+      return NextResponse.json({ ...project, skills, members, next: next[0], prev: prev[0] }, { status: 200 });
     } else {
-      return Response.json({ message: "slug does not exist" }, { status: 404 });
+      return NextResponse.json({ message: "slug does not exist" }, { status: 404 });
     }
   }
 }
--- app/api/project/list/route.ts
+++ app/api/project/list/route.ts
@@ -1,7 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { sql } from '@/lib/db-init';
-// import { getBaseUrl } from "@/lib/url";
-// import { Project } from "@/lib/types";
 
 export async function GET(request: NextRequest) {
   const req = request.nextUrl.searchParams;
@@ -14,24 +12,4 @@ export async function GET(request: NextRequest) {
   `);
 
   return NextResponse.json(result, { status: 200 });
-
-  // const baseUrl = getBaseUrl();
-  // const res = await fetch(`${baseUrl}/api/project-data`,
-  //   { headers: { 'Content-Type': 'application/json' } }
-  // ).then(res => res.json());
-
-  // const limit = (count && count < res.length) ? count : res.length;
-  // let data = [];
-  // for (let i = 0; i < limit; i++) {
-  //   const project: Project = res[i];
-  //   data.push({
-  //     slug: project.slug,
-  //     keyVisual: project.keyVisual,
-  //     name: project.name,
-  //     description: project.description,
-  //     skill: project.skill
-  //   });
-  // }
-
-  // return Response.json(data);
 }
